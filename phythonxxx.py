146335451ada68740174beba393ca3627bb539bb
rhari18 <166897707+rhari18@users.noreply.github.com>
2024-06-05 15:04:49 +0530

    first commit

---
+++ phythonxxx.py
@@ -0,0 +1,87 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+This is a sample output
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+This is a sample output
+This is a secomd output
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+This is a sample output
+This is a secomd output
+ Hello World
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+5
+John
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+Sally
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+Sally
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+HarikaranRavikumar
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+Harikaran Ravikumar
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+1
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+9
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+20
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+6.333333333333333
+>>> 
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+3.0
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+2.6666666666666665
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+8
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+8
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+<class 'int'>
+<class 'str'>
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+25.0
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+25.0
+{47.0}
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+enter your first name:Hariharan
+HelloHariharan
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+enter your first name:Hariharan
+Hello Hariharan
+
+======================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python312/hello.py =======================================================
+enter the firt_number:10
+enter the second_number:12
+22
+
+
+
+enter the first_number:11
+SyntaxError: invalid syntax
